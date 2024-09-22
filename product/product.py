00624651876d44d06e72740fef7ae8ad6239353e
RomulusMirauta <romulus.mirauta1@gmail.com>
2024-09-22 17:54:51 +0300

    emptied repo

--- product/product.py
+++
@@ -1,9 +0,0 @@
-class Product:
-    def __init__(self, product_id, name, price, quantity=1):
-        self.product_id = product_id
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-    def __str__(self):
-        return f"Produs: ID {self.product_id} Nume: {self.name} Pret: {self.price} Cantitate: {self.quantity}"
--- user/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, user_id, username, email, password):
-        self.user_id = user_id
-        self.username = username
-        self.email = email
-        self.password = password
-
-    def __str__(self):
-        return f"User: ID {self.user_id} Nume utilizator: {self.username} Email: {self.email} Password: {self.password}"
